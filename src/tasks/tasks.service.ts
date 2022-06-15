41d24fcc230874b9b93057c97dd49b483d0ee2d4
AlaaEl-DinAhmed <alaaeldin.muhammad@gmail.com>
2022-06-15 22:20:15 +0200

    cleaning task controller and service

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,36 +1,3 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
-import { v4 as uuid } from 'uuid';
-import { CreateTaskDto } from './dto/create-task.dto';
-import { ITask, TaskStatus } from './tasks.interface';
+import { Injectable } from '@nestjs/common';
 @Injectable()
-export class TasksService {
-  private tasks: ITask[] = [];
-
-  getAllTasks(): ITask[] {
-    return this.tasks;
-  }
-
-  getSingleTask(id: string): ITask {
-    const singleTask = this.tasks.find((task: ITask) => task.id === id);
-    if (!singleTask) {
-      throw new NotFoundException(`Task with id ${id} is not found.`);
-    }
-    return singleTask;
-  }
-
-  createTasks(createTaskDto: CreateTaskDto): ITask {
-    const { title, description } = createTaskDto;
-    const task: ITask = {
-      id: uuid(),
-      title,
-      description,
-      status: TaskStatus.OPEN,
-    };
-    this.tasks.push(task);
-    return task;
-  }
-
-  deleteTask(id: string): void {
-    this.tasks.filter((task: ITask) => task.id !== id);
-  }
-}
+export class TasksService {}
